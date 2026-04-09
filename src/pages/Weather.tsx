65fd63d49c34b2b8e341ea5f36c96618dce0c05a
jeadys <yassin.ao.work@gmail.com>
2026-04-09 11:10:20 +0200

    Add weather page

---
+++ src/pages/Weather.tsx
@@ -0,0 +1,9 @@
+import {Section} from "../components/Section";
+
+export const Weather = () => {
+    return (
+        <Section title="Weather" description="A nice list">
+            <></>
+        </Section>
+    )
+}
